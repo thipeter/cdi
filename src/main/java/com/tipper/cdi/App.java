c7e106a66826a65941d6379da88fb55f25ec467d
Thiago Peter <thiagopeter@gmail.com>
2016-06-19 16:45:12 -0700

    Initial commit

---
+++ src/main/java/com/tipper/cdi/App.java
@@ -0,0 +1,13 @@
+package com.tipper.cdi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
